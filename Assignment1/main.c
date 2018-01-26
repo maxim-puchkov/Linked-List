782cb551e9f16f52b26593648d5f3d4c68465953
mpuchkov <mpuchkov@sfu.ca>
2018-01-26 09:57:39 -0800

    Initial Commit

---
+++ Assignment1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Assignment1
+//
+//  Created by admin on 2018-01-26.
+//  Copyright © 2018 Maxim Puchkov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
